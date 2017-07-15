e8905f583f35f27f52092e33aadb38e8a33a996e
Allenxuxu <120582243@qq.com>
2017-07-15 10:21:36 +0800

    added

--- XXLib.pro
+++ XXLib.pro
@@ -24,4 +24,6 @@ HEADERS += \
     Pointer.h \
     SharedPointer.h \
     CircleList.h \
-    DualLinkList.h
+    DualLinkList.h \
+    LinuxList.h \
+    DualCircleList.h
--- main.cpp
+++ main.cpp
@@ -11,20 +11,21 @@
 #include "SharedPointer.h"
 #include "CircleList.h"
 #include "DualLinkList.h"
+#include "DualCircleList.h"
 using namespace std;
 using namespace XXLib;
 
 int main(void)
 {
-    DualLinkList<int> a;
+    DualCircleList<int> a;
     for(int i = 0; i < 5; i++)
     {
-       cout<< a.insert(i) << endl;;
+        a.insert(i);
         cout << i <<endl;
         //a.insert(1,1);
     }
     cout <<"length: " << a.length() << endl;
-    for(a.move(0); !a.end(); a.next())
+   for(a.move(a.length()-1); !a.end(); a.next())
     {
         cout << a.current() <<endl;
         a.remove(a.find(a.current()));
